0075038b2e06e4d06fd03764e43ce700a536fff5
hampu5 <halu1701@student.miun.se>
2021-12-02 17:31:48 +0100

    Added code for web3 instance in the app.js file

--- src/js/app.js
+++ src/js/app.js
@@ -24,8 +24,25 @@ App = {
   },
 
   initWeb3: async function() {
-    // Make the provider the remote Ganache Blockchain
-    App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      App.web3Provider = window.ethereum;
+      try {
+        // Request account access
+        await window.ethereum.request({ method: "eth_requestAccounts" });;
+      } catch (error) {
+        // User denied account access...
+        console.error("User denied account access")
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = window.web3.currentProvider;
+    }
+    // If no injected web3 instance is detected, fall back to Ganache
+    else {
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:8545');
+    }
     web3 = new Web3(App.web3Provider);
 
     return App.initContract();
